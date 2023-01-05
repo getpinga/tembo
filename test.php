c1edf4a673ba8630becb0a4c48678f80483ec18a
Pinga <121483313+getpinga@users.noreply.github.com>
2023-01-06 00:21:16 +0200

    Update test.php

--- test.php
+++ test.php
@@ -37,36 +37,82 @@ use Pinga\Tembo\Epp;
             'clID' => 'testregistrar1',
             'pw' => 'testpassword1',
             'prefix' => 'TESTR1'
+            'ext' => 'iis.se'
         ));
-
-
-        $checkparams = array(
-            'domains' => array('example1.com','example2.com')
+		
+		
+/*         $ccreateparams = array(
+            'id' => 'ABCTEST123',
+            'firstname' => 'Petko',
+            'lastname' => 'Petkov',
+            'companyname' => 'Petkovi OOD',
+            'address1' => 'bul. Vitosha 1',
+            'address2' => 'ap. 1',
+            'city' => 'Sofia',
+            'state' => 'Sofia-Grad',
+            'postcode' => '1000',
+            'country' => 'BG',
+            'fullphonenumber' => '+359.1234567',
+            'email' => 'test@petkovi.bg',
+            'orgno' => '0049132590',
+            'vatno' => ''
         );
-        $domainCheck = $epp->domainCheck($checkparams);
+        $contactCreate = $epp->contactCreateIIS($ccreateparams); */
+		
+/*          $hcreateparams = array(
+            'hostname' => 'ns2.example.com',
+            'v' => 'v4',
+            'ip' => '54.6.7.8'
+        );
+        $hostCreate = $epp->hostCreate($hcreateparams);  */
+		
+/* 		
+        $ccreateparams = array(
+            'id' => 'ABCTEST123',
+            'firstname' => 'Petko',
+            'lastname' => 'Petkov',
+            'companyname' => 'Petkovi OOD',
+            'address1' => 'bul. Vitosha 1',
+            'address2' => 'ap. 1',
+            'city' => 'Sofia',
+            'state' => 'Sofia-Grad',
+            'postcode' => '1000',
+            'country' => 'BG',
+            'fullphonenumber' => '+359.1234567',
+            'email' => 'test@petkovi.bg',
+            'authInfoPw' => 'ABCSofi@345'
+        );
+        $contactCreate = $epp->contactCreate($ccreateparams); */
 
 
-        $infoparams = array(
+/*         $checkparams = array(
+            'domains' => array('google123.test')
+        );
+        $domainCheck = $epp->domainCheck($checkparams); */
+
+
+/*         $infoparams = array(
             'domainname' => 'example1.com',
             'authInfoPw' => 'domainpw123@'
         );
         $domainInfo = $epp->domainInfo($infoparams);
-		
+		*/
  
         $createparams = array(
-            'domainname' => 'example1.com',
+            'domainname' => 'georgievi.test',
             'period' => 1,
             'nss' => array('ns1.example.com','ns2.example.com'),
-            'registrant' => 'EX-1234567',
-            'contacts' => array(
+            'registrant' => 'ABCTEST123',
+/*             'contacts' => array(
                 'EX-1234567' => 'admin',
                 'EX-1234567' => 'tech',
                 'EX-1234567' => 'billing'
-                ),
-            'authInfoPw' => 'domainpw123@'
+                ), */
+             'authInfoPw' => 'Domainpw123@',
+            'ext' => 'iis.se'
         );
-        $domainCreate = $epp->domainCreate($createparams);
-
+        $domainCreate = $epp->domainCreate($createparams); 
+/*
 
         $transferparams = array(
             'domainname' => 'example1.com',
@@ -80,29 +126,33 @@ use Pinga\Tembo\Epp;
             'domainname' => 'example1.com',
             'regperiod' => 1
         );
-        $domainRenew = $epp->domainRenew($renewparams);
+        $domainRenew = $epp->domainRenew($renewparams); */
     }
 
     catch (EppException $e) {
         echo 'Error: ', $e->getMessage();
     }
 
-
+//print_r($contactCreate);
+//print_r($hostCreate);
 //echo '<pre>';
 //print_r($domainCheck);
 //print_r($domainInfo);
-//print_r($domainCreate);
+print_r($domainCreate);
 //print_r($domainTransfer);
 //print_r($domainRenew);
 //echo '</pre>';
 
-foreach ($domainCheck['domains'] as $domain) {
+/* foreach ($domainCheck['domains'] as $domain) {
     // Check if the domain is available
     if ($domain['avail'] === 1) {
         echo "The domain " . $domain['name'] . " is available." . PHP_EOL;
     } else {
         echo "The domain " . $domain['name'] . " is not available. Reason: " . $domain['reason'] . PHP_EOL;
     }
-}
+} */
+
+        $logout = $epp->logout();
+		print_r($logout);
 
 ?>
