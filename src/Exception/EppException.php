944281e522980f4f21ef75a608f65cdd11dc4a16
Pinga <121483313+getpinga@users.noreply.github.com>
2023-01-05 18:13:54 +0200

    Create EppException.php

---
+++ src/Exception/EppException.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Tembo EPP client library
+ *
+ * Written in 2023 by Taras Kondratyuk (https://getpinga.com)
+ * Based on xpanel/epp-bundle written in 2019 by Lilian Rudenco (info@xpanel.com)
+ *
+ * @license MIT
+ */
+ 
+namespace Pinga\Tembo\Exception;
+
+class EppException extends \Exception
+{
+}
