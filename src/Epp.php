56f206a734b8ff38ae72b1e5b8452e045493fe33
Pinga <121483313+getpinga@users.noreply.github.com>
2023-01-06 00:21:46 +0200

    Update Epp.php

--- src/Epp.php
+++ src/Epp.php
@@ -7,7 +7,7 @@
  *
  * @license MIT
  */
- 
+
 namespace Pinga\Tembo; 
 use Pinga\Tembo\Exception\EppException;
 use Pinga\Tembo\Exception\EppNotConnectedException;
@@ -152,6 +152,13 @@ class Epp
             $to[] = htmlspecialchars($params['clID']);
             $from[] = '/{{ pwd }}/';
             $to[] = htmlspecialchars($params['pw']);
+			if ($params['ext'] == 'iis.se') {
+            $from[] = '/{{ extensions }}/';
+            $to[] = '<extURI>urn:se:iis:xml:epp:iis-1.2</extURI>';
+			} else {
+            $from[] = '/{{ extensions }}/';
+            $to[] = '';
+			}
             $from[] = '/{{ clTRID }}/';
             $microtime = str_replace('.', '', round(microtime(1), 3));
             $to[] = htmlspecialchars($params['prefix'] . '-login-' . $microtime);
@@ -173,6 +180,7 @@ class Epp
         <objURI>urn:ietf:params:xml:ns:host-1.0</objURI>
         <svcExtension>
           <extURI>urn:ietf:params:xml:ns:secDNS-1.1</extURI>
+		  {{ extensions }}
         </svcExtension>
       </svcs>
     </login>
@@ -248,6 +256,276 @@ class Epp
 
         return $return;
     }
+	
+    /**
+     * hostCreate
+     */
+    function hostCreate($params = array())
+    {
+        if (!$this->isLoggedIn) {
+            return array(
+                'code' => 2002,
+                'msg' => 'Command use error'
+            );
+        }
+
+        $return = array();
+        try {
+            $from = $to = array();
+            $from[] = '/{{ name }}/';
+            $to[] = htmlspecialchars($params['hostname']);
+            $from[] = '/{{ v }}/';
+            $to[] = htmlspecialchars($params['v']);
+            $from[] = '/{{ ip }}/';
+            $to[] = htmlspecialchars($params['ip']);
+            $from[] = '/{{ clTRID }}/';
+            $clTRID = str_replace('.', '', round(microtime(1), 3));
+            $to[] = htmlspecialchars($this->prefix . '-host-create-' . $clTRID);
+			$from[] = "/<\w+:\w+>\s*<\/\w+:\w+>\s+/ims";
+			$to[] = '';
+			$xml = preg_replace($from, $to, '<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+<epp xmlns="urn:ietf:params:xml:ns:epp-1.0"
+  xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+  xsi:schemaLocation="urn:ietf:params:xml:ns:epp-1.0 epp-1.0.xsd">
+  <command>
+	<create>
+	  <host:create
+	   xmlns:host="urn:ietf:params:xml:ns:host-1.0">
+		<host:name>{{ name }}</host:name>
+		<host:addr ip="{{ v }}">{{ ip }}</host:addr>
+	  </host:create>
+	</create>
+	<clTRID>{{ clTRID }}</clTRID>
+  </command>
+</epp>');
+            $r = $this->writeRequest($xml);
+            $code = (int)$r->response->result->attributes()->code;
+            $msg = (string)$r->response->result->msg;
+            $r = $r->response->resData->children('urn:ietf:params:xml:ns:host-1.0')->creData;
+            $name = (string)$r->name;
+
+            $return = array(
+                'code' => $code,
+                'msg' => $msg,
+                'name' => $name
+            );
+        }
+
+        catch(\Exception $e) {
+            $return = array(
+                'error' => $e->getMessage()
+            );
+        }
+
+        return $return;
+    }
+	
+    /**
+     * contactCreate
+     */
+    function contactCreate($params = array())
+    {
+        if (!$this->isLoggedIn) {
+            return array(
+                'code' => 2002,
+                'msg' => 'Command use error'
+            );
+        }
+
+        $return = array();
+        try {
+			$from = $to = array();
+			$from[] = '/{{ id }}/';
+			$to[] = htmlspecialchars($params['id']);
+			$from[] = '/{{ name }}/';
+			$to[] = htmlspecialchars($params['firstname'] . ' ' . $params['lastname']);
+			$from[] = '/{{ org }}/';
+			$to[] = htmlspecialchars($params['companyname']);
+			$from[] = '/{{ street1 }}/';
+			$to[] = htmlspecialchars($params['address1']);
+			$from[] = '/{{ street2 }}/';
+			$to[] = htmlspecialchars($params['address2']);
+			$from[] = '/{{ street3 }}/';
+			$street3 = (isset($params['address3']) ? $params['address3'] : '');
+			$to[] = htmlspecialchars($street3);
+			$from[] = '/{{ city }}/';
+			$to[] = htmlspecialchars($params['city']);
+			$from[] = '/{{ state }}/';
+			$to[] = htmlspecialchars($params['state']);
+			$from[] = '/{{ postcode }}/';
+			$to[] = htmlspecialchars($params['postcode']);
+			$from[] = '/{{ country }}/';
+			$to[] = htmlspecialchars($params['country']);
+			$from[] = '/{{ phonenumber }}/';
+			$to[] = htmlspecialchars($params['fullphonenumber']);
+			$from[] = '/{{ email }}/';
+			$to[] = htmlspecialchars($params['email']);
+            $from[] = '/{{ authInfo }}/';
+            $to[] = htmlspecialchars($params['authInfoPw']);
+            $from[] = '/{{ clTRID }}/';
+            $microtime = str_replace('.', '', round(microtime(1), 3));
+            $to[] = htmlspecialchars($this->prefix . '-contact-create-' . $microtime);	
+			$from[] = "/<\w+:\w+>\s*<\/\w+:\w+>\s+/ims";
+			$to[] = '';
+			$xml = preg_replace($from, $to, '<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+<epp xmlns="urn:ietf:params:xml:ns:epp-1.0"
+  xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+  xsi:schemaLocation="urn:ietf:params:xml:ns:epp-1.0 epp-1.0.xsd">
+  <command>
+	<create>
+	  <contact:create
+	   xmlns:contact="urn:ietf:params:xml:ns:contact-1.0">
+		<contact:id>{{ id }}</contact:id>
+		<contact:postalInfo type="int">
+		  <contact:name>{{ name }}</contact:name>
+		  <contact:org>{{ org }}</contact:org>
+		  <contact:addr>
+			<contact:street>{{ street1 }}</contact:street>
+			<contact:street>{{ street2 }}</contact:street>
+			<contact:street>{{ street3 }}</contact:street>
+			<contact:city>{{ city }}</contact:city>
+			<contact:sp>{{ state }}</contact:sp>
+			<contact:pc>{{ postcode }}</contact:pc>
+			<contact:cc>{{ country }}</contact:cc>
+		  </contact:addr>
+		</contact:postalInfo>
+		<contact:voice>{{ phonenumber }}</contact:voice>
+		<contact:fax></contact:fax>
+		<contact:email>{{ email }}</contact:email>
+		<contact:authInfo>
+		  <contact:pw>{{ authInfo }}</contact:pw>
+		</contact:authInfo>
+	  </contact:create>
+	</create>
+	{{ extensions }}
+	<clTRID>{{ clTRID }}</clTRID>
+  </command>
+</epp>');
+            $r = $this->writeRequest($xml);
+            $code = (int)$r->response->result->attributes()->code;
+            $msg = (string)$r->response->result->msg;
+            $r = $r->response->resData->children('urn:ietf:params:xml:ns:contact-1.0')->creData;
+            $id = (string)$r->id;
+
+            $return = array(
+                'code' => $code,
+                'msg' => $msg,
+                'id' => $id
+            );
+        }
+
+        catch(\Exception $e) {
+            $return = array(
+                'error' => $e->getMessage()
+            );
+        }
+
+        return $return;
+    }
+	
+    /**
+     * contactCreateIIS
+     */
+    function contactCreateIIS($params = array())
+    {
+        if (!$this->isLoggedIn) {
+            return array(
+                'code' => 2002,
+                'msg' => 'Command use error'
+            );
+        }
+
+        $return = array();
+        try {
+			$from = $to = array();
+			$from[] = '/{{ id }}/';
+			$to[] = htmlspecialchars($params['id']);
+			$from[] = '/{{ name }}/';
+			$to[] = htmlspecialchars($params['firstname'] . ' ' . $params['lastname']);
+			$from[] = '/{{ org }}/';
+			$to[] = htmlspecialchars($params['companyname']);
+			$from[] = '/{{ street1 }}/';
+			$to[] = htmlspecialchars($params['address1']);
+			$from[] = '/{{ street2 }}/';
+			$to[] = htmlspecialchars($params['address2']);
+			$from[] = '/{{ street3 }}/';
+			$street3 = (isset($params['address3']) ? $params['address3'] : '');
+			$to[] = htmlspecialchars($street3);
+			$from[] = '/{{ city }}/';
+			$to[] = htmlspecialchars($params['city']);
+			$from[] = '/{{ state }}/';
+			$to[] = htmlspecialchars($params['state']);
+			$from[] = '/{{ postcode }}/';
+			$to[] = htmlspecialchars($params['postcode']);
+			$from[] = '/{{ country }}/';
+			$to[] = htmlspecialchars($params['country']);
+			$from[] = '/{{ phonenumber }}/';
+			$to[] = htmlspecialchars($params['fullphonenumber']);
+			$from[] = '/{{ email }}/';
+			$to[] = htmlspecialchars($params['email']);
+            $from[] = '/{{ clTRID }}/';
+            $microtime = str_replace('.', '', round(microtime(1), 3));
+            $to[] = htmlspecialchars($this->prefix . '-contact-create-' . $microtime);	
+			$from[] = "/<\w+:\w+>\s*<\/\w+:\w+>\s+/ims";
+			$to[] = '';
+			$xml = preg_replace($from, $to, '<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+<epp xmlns="urn:ietf:params:xml:ns:epp-1.0"
+  xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+  xsi:schemaLocation="urn:ietf:params:xml:ns:epp-1.0 epp-1.0.xsd">
+  <command>
+	<create>
+	  <contact:create
+	   xmlns:contact="urn:ietf:params:xml:ns:contact-1.0">
+		<contact:id>{{ id }}</contact:id>
+		<contact:postalInfo type="loc">
+		  <contact:name>{{ name }}</contact:name>
+		  <contact:org>{{ org }}</contact:org>
+		  <contact:addr>
+			<contact:street>{{ street1 }}</contact:street>
+			<contact:street>{{ street2 }}</contact:street>
+			<contact:street>{{ street3 }}</contact:street>
+			<contact:city>{{ city }}</contact:city>
+			<contact:sp>{{ state }}</contact:sp>
+			<contact:pc>{{ postcode }}</contact:pc>
+			<contact:cc>{{ country }}</contact:cc>
+		  </contact:addr>
+		</contact:postalInfo>
+		<contact:voice>{{ phonenumber }}</contact:voice>
+		<contact:fax></contact:fax>
+		<contact:email>{{ email }}</contact:email>
+	  </contact:create>
+	</create>
+	<extension>
+	  <iis:create xmlns:iis="urn:se:iis:xml:epp:iis-1.2"
+	  xsi:schemaLocation="urn:se:iis:xml:epp:iis-1.2 iis-1.2.xsd">
+	    <iis:orgno>[{{ country }}]{{ orgno }}</iis:orgno>
+	    <iis:vatno>{{ vatno }}</iis:vatno>
+	  </iis:create>
+	</extension>
+	<clTRID>{{ clTRID }}</clTRID>
+  </command>
+</epp>');
+            $r = $this->writeRequest($xml);
+            $code = (int)$r->response->result->attributes()->code;
+            $msg = (string)$r->response->result->msg;
+            $r = $r->response->resData->children('urn:ietf:params:xml:ns:contact-1.0')->creData;
+            $id = (string)$r->id;
+
+            $return = array(
+                'code' => $code,
+                'msg' => $msg,
+                'id' => $id
+            );
+        }
+
+        catch(\Exception $e) {
+            $return = array(
+                'error' => $e->getMessage()
+            );
+        }
+
+        return $return;
+    }
 
     /**
      * domainCheck
@@ -273,6 +551,8 @@ class Epp
             $from[] = '/{{ clTRID }}/';
             $microtime = str_replace('.', '', round(microtime(1), 3));
             $to[] = htmlspecialchars($this->prefix . '-domain-check-' . $microtime);
+			$from[] = "/<\w+:\w+>\s*<\/\w+:\w+>\s+/ims";
+			$to[] = '';
             $xml = preg_replace($from, $to, '<?xml version="1.0" encoding="UTF-8" standalone="no"?>
 <epp xmlns="urn:ietf:params:xml:ns:epp-1.0"
   xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
@@ -339,6 +619,8 @@ class Epp
             $from[] = '/{{ clTRID }}/';
             $microtime = str_replace('.', '', round(microtime(1), 3));
             $to[] = htmlspecialchars($this->prefix . '-domain-info-' . $microtime);
+			$from[] = "/<\w+:\w+>\s*<\/\w+:\w+>\s+/ims";
+			$to[] = '';
             $xml = preg_replace($from, $to, '<?xml version="1.0" encoding="UTF-8" standalone="no"?>
 <epp xmlns="urn:ietf:params:xml:ns:epp-1.0"
   xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
@@ -450,6 +732,8 @@ class Epp
             $from[] = '/{{ clTRID }}/';
             $clTRID = str_replace('.', '', round(microtime(1), 3));
             $to[] = htmlspecialchars($this->prefix . '-domain-transfer-' . $clTRID);
+			$from[] = "/<\w+:\w+>\s*<\/\w+:\w+>\s+/ims";
+			$to[] = '';
 		    $xml = preg_replace($from, $to, '<?xml version="1.0" encoding="UTF-8" standalone="no"?>
 <epp xmlns="urn:ietf:params:xml:ns:epp-1.0"
   xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
@@ -529,12 +813,17 @@ class Epp
             $to[] = $text;
             $from[] = '/{{ registrant }}/';
             $to[] = htmlspecialchars($params['registrant']);
+			if ($params['ext'] == 'iis.se') {
+            $from[] = '/{{ contacts }}/';
+            $to[] = '';
+			} else {
             $text = '';
             foreach ($params['contacts'] as $id => $contactType) {
                 $text .= '<domain:contact type="' . $contactType . '">' . $id . '</domain:contact>' . "\n";
             }
             $from[] = '/{{ contacts }}/';
             $to[] = $text;
+			}
             $from[] = '/{{ authInfoPw }}/';
             $to[] = htmlspecialchars($params['authInfoPw']);
             $from[] = '/{{ clTRID }}/';
@@ -611,6 +900,8 @@ class Epp
             $from[] = '/{{ clTRID }}/';
             $clTRID = str_replace('.', '', round(microtime(1), 3));
             $to[] = htmlspecialchars($this->prefix . '-domain-info-' . $clTRID);
+			$from[] = "/<\w+:\w+>\s*<\/\w+:\w+>\s+/ims";
+			$to[] = '';
 		    $xml = preg_replace($from, $to, '<?xml version="1.0" encoding="UTF-8" standalone="no"?>
 <epp xmlns="urn:ietf:params:xml:ns:epp-1.0"
   xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
