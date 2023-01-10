246b93389803da3b83b774295c6879074ff75fb7
Pinga <121483313+getpinga@users.noreply.github.com>
2023-01-10 18:56:43 +0200

    Create DomainUpdateNS.php

---
+++ examples/DomainUpdateNS.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Tembo EPP client test file
+ *
+ * Written in 2023 by Taras Kondratyuk (https://getpinga.com)
+ * Based on xpanel/epp-bundle written in 2019 by Lilian Rudenco (info@xpanel.com)
+ *
+ * @license MIT
+ */
+
+// Include the Composer autoloader
+require_once '../vendor/autoload.php';
+require_once 'Connection.php';
+
+// Use the Epp class from your package
+use Pinga\Tembo\Epp;
+use Pinga\Tembo\EppClient;
+use Pinga\Tembo\HttpsClient;
+
+try
+{
+    $epp = connectEpp();
+
+    $params = array(
+        'domainname' => 'test.example',
+        'ns1' => 'ns1.example.com',
+        'ns2' => 'ns2.example.com'
+    );
+    $domainUpdateNS = $epp->domainUpdateNS($params);
+	
+    if (array_key_exists('error', $domainUpdateNS))
+    {
+        echo 'DomainUpdateNS Error: ' . $domainUpdateNS['error'] . PHP_EOL;
+    }
+    else
+    {
+        echo "DomainUpdateNS result: " . $domainUpdateNS['code'] . ": " . $domainUpdateNS['msg'] . PHP_EOL;
+    }
+	
+    $logout = $epp->logout();
+
+    echo 'Logout Result: ' . $logout['code'] . ': ' . $logout['msg'][0] . PHP_EOL;
+}
+catch(EppException $e)
+{
+    echo 'Error: ', $e->getMessage();
+}
+?>
