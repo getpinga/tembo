594feb21df64f7bb5f68eac5f9083c7254abaf76
Pinga <121483313+getpinga@users.noreply.github.com>
2023-02-16 19:53:21 +0200

    Create KeepAlive.php

---
+++ examples/KeepAlive.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Tembo EPP client test file
+ *
+ * Written in 2023 by Taras Kondratyuk (https://getpinga.com)
+ * Based on xpanel/epp-bundle written in 2019 by Lilian Rudenco (info@xpanel.com)
+ *
+ * @license MIT
+ */
+
+// Include the Composer autoloader
+require_once '../vendor/autoload.php';
+require_once 'Connection.php';
+
+// Use the Epp class from your package
+use Pinga\Tembo\Epp;
+use Pinga\Tembo\EppClient;
+use Pinga\Tembo\HttpsClient;
+
+try
+{
+    $epp = connectEpp();
+	
+    $startTime = time();
+    $endTime = $startTime + 1800; // 30 minutes from start time
+
+    while (time() < $endTime) {
+        $hello = $epp->hello();
+        echo 'Keep-alive Result: ' . PHP_EOL . $hello . PHP_EOL;
+        sleep(600); // wait for 10 minutes
+    }
+	
+    $logout = $epp->logout();
+    echo 'Logout Result: ' . $logout['code'] . ': ' . $logout['msg'][0] . PHP_EOL;
+}
+catch(EppException $e)
+{
+    echo 'Error: ', $e->getMessage();
+}
