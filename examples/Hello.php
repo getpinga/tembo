09104d6ec27953a9d8e9e33e041e9e5cae53e2f6
Pinga <121483313+getpinga@users.noreply.github.com>
2023-02-16 19:43:22 +0200

    Create Hello.php

---
+++ examples/Hello.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Tembo EPP client test file
+ *
+ * Written in 2023 by Taras Kondratyuk (https://getpinga.com)
+ * Based on xpanel/epp-bundle written in 2019 by Lilian Rudenco (info@xpanel.com)
+ *
+ * @license MIT
+ */
+
+// Include the Composer autoloader
+require_once '../vendor/autoload.php';
+require_once 'Connection.php';
+
+// Use the Epp class from your package
+use Pinga\Tembo\Epp;
+use Pinga\Tembo\EppClient;
+use Pinga\Tembo\HttpsClient;
+
+try
+{
+    $epp = connectEpp();
+
+    $hello = $epp->hello();
+    echo 'Hello Result: ' . PHP_EOL . $hello . PHP_EOL;
+	
+    $logout = $epp->logout();
+    echo 'Logout Result: ' . $logout['code'] . ': ' . $logout['msg'][0] . PHP_EOL;
+}
+catch(EppException $e)
+{
+    echo 'Error: ', $e->getMessage();
+}
