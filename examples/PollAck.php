a15327464da8cd531abe02e055f58dce1f8caebd
Pinga <121483313+getpinga@users.noreply.github.com>
2023-02-17 12:30:20 +0200

    Added Poll

---
+++ examples/PollAck.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Tembo EPP client test file
+ *
+ * Written in 2023 by Taras Kondratyuk (https://getpinga.com)
+ * Based on xpanel/epp-bundle written in 2019 by Lilian Rudenco (info@xpanel.com)
+ *
+ * @license MIT
+ */
+
+// Include the Composer autoloader
+require_once '../vendor/autoload.php';
+require_once 'Connection.php';
+
+// Use the Epp class from your package
+use Pinga\Tembo\Epp;
+use Pinga\Tembo\EppClient;
+use Pinga\Tembo\HttpsClient;
+
+try
+{
+    $epp = connectEpp();
+
+    $params = array(
+        'msgID' => '12345'
+    );
+    $pollAck = $epp->pollAck($params);
+	
+	if (isset($pollAck['error'])) {
+    echo 'Error: ' . $pollAck['error'] . PHP_EOL;
+	} else {
+    echo 'Poll Ack Result: ' . $pollAck['code'] . ': ' . $pollAck['msg'] . PHP_EOL;
+	}
+	
+    $logout = $epp->logout();
+    echo 'Logout Result: ' . $logout['code'] . ': ' . $logout['msg'][0] . PHP_EOL;
+}
+catch(EppException $e)
+{
+    echo 'Error: ', $e->getMessage();
+}
