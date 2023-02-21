62ee978b8d707c3a9fa260b187f674edfb9914b0
Pinga <121483313+getpinga@users.noreply.github.com>
2023-02-21 17:17:44 +0200

    Create DomainCreateDNSSEC.php

---
+++ examples/DomainCreateDNSSEC.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * Tembo EPP client test file
+ *
+ * Written in 2023 by Taras Kondratyuk (https://getpinga.com)
+ * Based on xpanel/epp-bundle written in 2019 by Lilian Rudenco (info@xpanel.com)
+ *
+ * @license MIT
+ */
+
+// Include the Composer autoloader
+require_once '../vendor/autoload.php';
+require_once 'Connection.php';
+
+try
+{
+	$epp = connectEpp('generic');
+	
+    $params = array(
+        'domainname' => 'test.example',
+        'period' => 1,
+        'nss' => array('ns1.google.com','ns2.goolge.com'),
+        'registrant' => 'tembo005',
+        'contacts' => array(
+           'tembo005' => 'admin',
+           'tembo005' => 'tech',
+           'tembo005' => 'billing'
+        ), 
+        'authInfoPw' => 'Domainpw123@',
+        'dnssec_records' => '2',
+        'keyTag_1' => '33409',
+        'alg_1' => '8',
+        'digestType_1' => '1',
+        'digest_1' => 'F4D6E26B3483C3D7B3EE17799B0570497FAF33BCB12B9B9CE573DDB491E16948',
+        'keyTag_2' => '43409',
+        'alg_2' => '8',
+        'digestType_2' => '2',
+        'digest_2' => 'F3D6E26B3483C3D7B3EE17799B0570497FAF33BCB12B9B9CE573DDB491E16564'
+    );
+    $domainCreateDNSSEC = $epp->domainCreateDNSSEC($params);
+
+    if (array_key_exists('error', $domainCreateDNSSEC))
+    {
+        echo 'DomainCreateDNSSEC Error: ' . $domainCreateDNSSEC['error'] . PHP_EOL;
+    }
+    else
+    {
+        echo 'DomainCreateDNSSEC Result: ' . $domainCreateDNSSEC['code'] . ': ' . $domainCreateDNSSEC['msg'] . PHP_EOL;
+		echo 'New Domain: ' . $domainCreateDNSSEC['name'] . PHP_EOL;
+		echo 'Created On: ' . $domainCreateDNSSEC['crDate'] . PHP_EOL;
+		echo 'Expires On: ' . $domainCreateDNSSEC['exDate'] . PHP_EOL;
+    }
+
+    $logout = $epp->logout();
+
+    echo 'Logout Result: ' . $logout['code'] . ': ' . $logout['msg'][0] . PHP_EOL;
+}
+catch(EppException $e)
+{
+    echo 'Error: ', $e->getMessage();
+}
