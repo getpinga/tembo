20a2438ea0776d36154101497345cb9bac71244c
Pinga <121483313+getpinga@users.noreply.github.com>
2023-12-12 12:25:45 +0200

    Create DomainCreateSunrise.php

---
+++ examples/DomainCreateSunrise.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Tembo EPP client test file
+ *
+ * Written in 2023 by Taras Kondratyuk (https://getpinga.com)
+ * Based on xpanel/epp-bundle written in 2019 by Lilian Rudenco (info@xpanel.com)
+ *
+ * @license MIT
+ */
+
+// Include the Composer autoloader
+require_once '../vendor/autoload.php';
+require_once 'Connection.php';
+
+try
+{
+	$epp = connectEpp('generic');
+	
+    $params = array(
+        'domainname' => 'test.example',
+        'period' => 1,
+        'registrant' => 'tembo005',
+        'contacts' => array(
+           'admin' => 'tembo005',
+           'tech' => 'tembo005'
+        ),
+        'authInfoPw' => 'Domainpw123@',
+        'encodedSignedMark' => 'INSERT'
+    );
+    $domainCreateSunrise = $epp->domainCreateSunrise($params);
+
+    if (array_key_exists('error', $domainCreateSunrise))
+    {
+        echo 'DomainCreateSunrise Error: ' . $domainCreateSunrise['error'] . PHP_EOL;
+    }
+    else
+    {
+        echo 'DomainCreateSunrise Result: ' . $domainCreateSunrise['code'] . ': ' . $domainCreateSunrise['msg'] . PHP_EOL;
+		echo 'New Domain: ' . $domainCreateSunrise['name'] . PHP_EOL;
+		echo 'Created On: ' . $domainCreateSunrise['crDate'] . PHP_EOL;
+		echo 'Expires On: ' . $domainCreateSunrise['exDate'] . PHP_EOL;
+    }
+
+    $logout = $epp->logout();
+
+    echo 'Logout Result: ' . $logout['code'] . ': ' . $logout['msg'][0] . PHP_EOL;
+}
+catch(EppException $e)
+{
+    echo 'Error: ', $e->getMessage();
+}
